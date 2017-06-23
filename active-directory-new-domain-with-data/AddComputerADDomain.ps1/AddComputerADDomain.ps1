4f6da5b14643f63c92f64b548f09e1309da06c72
rcarboneras <rcarboneras@outlook.com>
2017-06-23 13:13:02 +0200

    Upload

---
+++ active-directory-new-domain-with-data/AddComputerADDomain.ps1/AddComputerADDomain.ps1
@@ -0,0 +1,54 @@
+﻿Configuration ADtotheDomain
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds
+
+   ) 
+        
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xComputerManagement
+    Import-DscResource -ModuleName xDSCFirewall
+            
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("$DomainName\$($AdminCreds.UserName)",$AdminCreds.Password)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+            AllowModuleOverWrite = $true
+        }
+
+        Service Winrm
+        {
+            Name = "Winrm"
+            StartupType = "Automatic"
+            State = "Running"
+        }
+
+        xComputer addtodomain
+        {
+            Name = $env:COMPUTERNAME
+            Credential = $DomainCreds
+            DomaiNName = $DomainName
+             
+        }
+        xDSCFirewall DisableFirewall
+        {
+            Ensure = "Absent"
+            Zone = "Domain"
+
+        }
+
+    }
+
+}
+
+
