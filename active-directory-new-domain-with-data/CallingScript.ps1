bb747ac393d2c90dbc431e063199db3482cd491e
rcarboneras <rcarboneras@outlook.com>
2017-06-26 15:51:55 +0200

    Update script

--- active-directory-new-domain-with-data/CallingScript.ps1
+++ active-directory-new-domain-with-data/CallingScript.ps1
@@ -1,9 +1,15 @@
-﻿break
-
+﻿
 # Shout out to @brwilkinson for assistance with some of this.
 
+Param (
+[switch]$SkipAzureModuleInstall
+)
 
+if (-not $SkipAzureModuleInstall)
+
+{
 # Install the Azure Resource Manager modules from PowerShell Gallery
+
 # Takes a while to install 28 modules
 Install-Module AzureRM -Force -Verbose
 Install-AzureRM
@@ -16,56 +22,93 @@ Import-AzureRM -Verbose
 
 # Import Azure Service Management module
 Import-Module Azure -Verbose
+}
+
 
 # Authenticate to your Azure account
+
 Login-AzureRmAccount
 
+
+
 # Adjust the 'yournamehere' part of these three strings to
+
 # something unique for you. Leave the last two characters in each.
+
 $URI       = 'https://raw.githubusercontent.com/rcarboneras/Psdemotraining/master/active-directory-new-domain-with-data/azuredeploy.json'
+
 $Location  = 'West Europe'
+
 $rgname    = 'yournamehererg'
+
 $saname    = 'yournameheresa'     # Lowercase required
 
+
+
 # Create the new resource group. Runs quickly.
+
 New-AzureRmResourceGroup -Name $rgname -Location $Location
 
+
+
 # Parameters for the template and configuration
+
 $MyParams = @{
+
     newStorageAccountName = $saname
+
     location              = 'West Europe'
+
     domainName            = 'contoso.com'
+
    }
+
+
 
 # Splat the parameters on New-AzureRmResourceGroupDeployment  
+
 $SplatParams = @{
+
     TemplateUri             = $URI 
+
     ResourceGroupName       = $rgname 
+
     TemplateParameterObject = $MyParams
-    Name                    = 'AlpineSkiHouseForest'
+
+    Name                    = 'PSDemotraining'
+
    }
 
+
+
 # This takes ~30 minutes
+
 # One prompt for the domain admin password
+
 New-AzureRmResourceGroupDeployment @SplatParams -Verbose
 
+
+
 # Find the VM IP and FQDN
-$PublicAddress = (Get-AzureRmPublicIpAddress -ResourceGroupName $rgname)[0]
+
+$PublicAddress = (Get-AzureRmPublicIpAddress -ResourceGroupName $rgname) | Where-Object -Property Name -EQ "W8publicIP"
+
 $IP   = $PublicAddress.IpAddress
 
 
+
+
+
 # RDP either way
+
 Start-Process -FilePath mstsc.exe -ArgumentList "/v:$IP"
 
+
+
 # Login as:  contoso\administrator
+
 # Use the password you supplied at the beginning of the build.
 
-# Explore the Active Directory domain:
-#  Recycle bin enabled
-#  Admin tools installed
-#  Five new OU structures
-#  Users and populated groups within the OU structures
-#  Users root container has test users and populated test groups
-
 # Delete the entire resource group when finished
-Remove-AzureRmResourceGroup -Name $rgname -Force -Verbose
+
+#Remove-AzureRmResourceGroup -Name $rgname -Force -Verbose
